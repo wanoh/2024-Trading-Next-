dd4171bab6df2a8873939535b359938452908445
Samuel Tetteh <khobbylynx55@gmail.com>
2024-03-26 23:24:56 +0000

    home route

--- src/layouts/components/acl/getHomeRoute.tsx
+++ src/layouts/components/acl/getHomeRoute.tsx
@@ -3,7 +3,7 @@
  */
 const getHomeRoute = (role: string) => {
   if (role === 'client') return '/acl'
-  else return '/dashboards/analytics'
+  else return '/trading-terminal'
 }
 
 export default getHomeRoute
